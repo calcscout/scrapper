41c7acbe8c3da679bc11442a0901036c5a941bad
calcscout <calcscout@gmail.com>
2021-09-14 15:52:21 +0200

    Bot is working!

--- pages/api/check-doodle.js
+++ pages/api/check-doodle.js
@@ -1,5 +1,8 @@
 const puppeteer = require('puppeteer-core');
 const chrome = require('chrome-aws-lambda');
+const axios = require('axios');
+
+const URI = `https://api.telegram.org/bot${process.env.BOT_ID}/sendMessage?chat_id=${process.env.CHAT_ID}&text=`;
 
 export default async function checkDoodle(_req, res) {
 	const browser = await puppeteer.launch(
@@ -11,19 +14,42 @@ export default async function checkDoodle(_req, res) {
 			  }
 			: { headless: false }
 	);
+	let text = undefined;
 	try {
 		const page = await browser.newPage();
 		await page.goto(`https://doodle.com/mm/nms/anmeldung`, {
 			waitUntil: 'networkidle0',
 		});
 
-		const text = await page.evaluate(() => {
-			return document.querySelector('body').innerText;
+		text = await page.evaluate(() => {
+			return document.querySelector('body').innerText.substr(0, 35);
 		});
 		await browser.close();
-		console.log(text);
-		res.status(200).send({ text });
 	} catch (error) {
 		res.status(error.status || 500).end(error.message);
 	}
+
+	const defaultAnswer = 'This Bookable Calendar link is off1.';
+	console.log(defaultAnswer, text);
+	if (defaultAnswer === text) {
+		text = 'No Changea to the Booking Link';
+	} else {
+		text =
+			'!!!Please check the link, something has changed!!! \n https://doodle.com/mm/nms/anmeldung';
+	}
+
+	const path = `${URI}${text}`;
+
+	axios
+		.get(path)
+		.then(function (response) {
+			// handle success
+			console.log(response);
+		})
+		.catch(function (error) {
+			// handle error
+			console.log(error);
+		});
+
+	res.status(200).send({ text });
 }
